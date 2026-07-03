e3a5f16a517b192e6ca13d4d1f01a92bfc3c1981
ayesha-rahman37 <ayesha30089@gmail.com>
2026-07-03 23:32:18 +0600

    Resolved error after getting update

--- devices/urls.py
+++ devices/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter  # type: ignore[reportMissingImports]
 from .views import RoomViewSet, DeviceViewSet, AlertViewSet
-from .views import RoomViewSet, DeviceViewSet, AlertViewSet, DeviceLogViewSet, dashboard
 
 router = DefaultRouter()
 router.register(r'rooms', RoomViewSet, basename='room')
 router.register(r'devices', DeviceViewSet, basename='device')
 router.register(r'alerts', AlertViewSet, basename='alert')
-router.register(r'logs', DeviceLogViewSet, basename='log')
 
 urlpatterns = [
-    path('api/', include(router.urls)),
-    path('', dashboard, name='dashboard'),  
+    path('', include(router.urls)),
 ]
